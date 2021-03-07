ca71e80bb4c16ecaa398c4005c7261ac82a940a3
Shubham Lad <shubhamlad1001@gmail.com>
2021-03-07 17:59:32 +0530

    API URL Change

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const fetchShorts = async (category) => {
   return new Promise((resolve, reject) => {
     axios
-      .get(`http://192.168.0.189:3000/shorts?category=${category}`)
+      .get(`https://short-news-api.herokuapp.com/shorts?category=${category}`)
       .then((response) => {
         console.log('Resopnse from fetch : ', response.data);
         resolve(response);
